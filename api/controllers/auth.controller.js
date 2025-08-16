07428b3c5d218c76f266928506358cd860e12e0a
lookmans.ai <Lookmans.007@gmail.com>
2025-08-16 02:51:19 +0200

    creating a middlewares and a function to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   // Perform signup logic here
   const hashPassword = bcrypt.hashSync(password, 10);
@@ -17,7 +18,10 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: "User created successfully!" });
   } catch (error) {
-    console.error("Error saving user:", error);
-    return res.status(500).json({ message: "Internal server error" });
+    if (error.code === 11000) {
+      next(errorHandler(400, "Username or email already exists."));
+    } else {
+      next(errorHandler(500, "Internal Server Error"));
+    }
   }
 };
--- api/index.js
+++ api/index.js
@@ -19,6 +19,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
+// middleware
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    status: "error",
+    statusCode,
+    message,
+  });
+});
+
 const port = process.env.PORT || 1024;
 
 app.listen(port, () => {
